59f938dcd0d0f8864155790c1d0b54e38111bf3a
rbruno22 <rbruno@libgot.com>
2026-05-31 16:50:44 -0300

    Add client and CLI de debug

--- client/client.h
+++ client/client.h
@@ -1,13 +1,54 @@
 #ifndef CLIENT_H
 #define CLIENT_H
 
+#include <cstdint>
+#include <memory>
+#include <mutex>
 #include <string>
+
+#include "common/direction.h"
+#include "common/queue.h"
+#include "common/socket.h"
+#include "common/thread.h"
+#include "common/updates/game_update.h"
+
+#include "client_protocol.h"
 
 class Client {
  private:
-    //...
+    Socket socket;
+    ClientProtocol protocol;
+
+    std::mutex send_mutex;
+
+    Queue<std::unique_ptr<GameUpdate>> received_updates;
+
+    std::unique_ptr<Thread> receiver_thread;
+
  public:
-    //...
+    Client(const std::string& host, const std::string& port);
+
+    void start();
+
+    void stop();
+
+    void join();
+
+    void do_login(const std::string& nick);
+    void do_list_matches();
+    void do_create_match(const std::string& name, uint8_t max_players);
+    void do_join_match(uint32_t match_id);
+    void do_select_race_class(uint8_t race, uint8_t klass);
+    void do_move(Direction dir);
+    void do_leave_match();
+    void do_disconnect();
+
+    Queue<std::unique_ptr<GameUpdate>>& get_received_updates();
+
+    ~Client();
+
+    Client(const Client&) = delete;
+    Client& operator=(const Client&) = delete;
 };
 
 #endif
--- server/server.h
+++ server/server.h
@@ -3,27 +3,60 @@
 
 #include <atomic>
 #include <cstdint>
+#include <list>
+#include <memory>
 #include <mutex>
 #include <string>
+#include <unordered_map>
+#include <unordered_set>
 #include <vector>
 
-#include "../common/queue.h"
-#include "../common/thread.h"
+#include "common/socket.h"
+#include "common/updates/match_list_update.h" 
+#include "game/match.h"
+#include "server_ops.h"
 
-class GameLoopThread;
+class AcceptorThread;
+class PlayerConnection;
 
-
-class Server {
+class Server: public ServerOps {
  private:
-    //...
+    std::string service_name;  
+
+    std::unique_ptr<Socket> listener;
+    std::unique_ptr<AcceptorThread> acceptor;
+
     mutable std::mutex clients_mutex;
+    std::list<PlayerConnection*> clients;
+    std::unordered_set<std::string> nicks_in_use;
+
+    mutable std::mutex matches_mutex;
+    std::unordered_map<uint32_t, std::unique_ptr<Match>> matches;
+    std::atomic<uint32_t> next_match_id;
+    std::atomic<uint32_t> next_player_id;
+
     std::atomic<bool> keep_running;
 
  public:
-    // ...
+    explicit Server(const std::string& service_name);
+
     void run();
 
-    ~Server() = default;
+    uint32_t login(PlayerConnection& conn, const std::string& nick) override;
+    std::vector<MatchInfo> list_matches() override;
+    uint32_t create_match(const std::string& name, uint8_t max_players,
+                          PlayerConnection& conn) override;
+    Match* join_match(uint32_t match_id, PlayerConnection& conn) override;
+    void leave_match(PlayerConnection& conn) override;
+    void disconnect(PlayerConnection& conn) override;
+
+    void add_client(PlayerConnection* conn);
+    void remove_client(PlayerConnection* conn);
+
+    ~Server() override;
+
+    Server(const Server&) = delete;
+    Server& operator=(const Server&) = delete;
 };
 
 #endif
