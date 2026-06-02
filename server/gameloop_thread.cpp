ddea3262db20ed446edb6e7b9c5e0e0949ba35c3
paula <pconsoli@fi.uba.ar>
2026-06-02 15:44:30 -0300

    fix: agrego comments

--- server/gameloop_thread.cpp
+++ server/gameloop_thread.cpp
@@ -13,7 +13,11 @@ GameLoopThread::GameLoopThread(Server& server, World& world): world(world), serv
 void GameLoopThread::run() {
     try {
         while (should_keep_running()) {
-            server.for_each_match([this](Match& match) { match.tick(world); });
+            server.for_each_match([this](Match& match) { match.tick(world); });  //
+            // en cada iteración del game loop se llama a match.tick(world) sobre cada partida
+            // activa luego cada match.tick(world) se encarga de procesar los comandos recibidos
+            // para esa partida y actualizar el estado de la partida y del mundo en consecuencia
+
 
             int sleep_ms = GameConfig::get_instance().get_server_game_loop_sleep_ms();
             std::this_thread::sleep_for(std::chrono::milliseconds(sleep_ms));
