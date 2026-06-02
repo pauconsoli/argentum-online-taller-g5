d22d28934afcdc66813c7ac0b9120ecdf4cf785d
paula <pconsoli@fi.uba.ar>
2026-06-02 02:17:24 -0300

    feat: agrego carga desde toml para constantes y valores del juego
    - Todas (creo) las constantes numéricas del juego en GameConfig + game_config.toml, sacando los valores hardcodeados
    - Agrego GameConfigLoader para parsear el toml e inicializar el config con esos valores, llamandolo desde main
    - Uso los getters en GameConfig para acceder a las constantes en la lógica del proyecto (server)
    - Reemplazo constantes donde corresponde, usando el GameConfig

--- server/game/game_formulas.cpp
+++ server/game/game_formulas.cpp
@@ -5,14 +5,14 @@
 #include "game_config.h"
 #include "player.h"
 
+// casteo a int porque tiene más sentido que los resultados sean enteros, pero se puede cambiar
+
 int GameFormulas::calculate_max_hp(const Player& player) {
     const GameConfig& config = GameConfig::get_instance();
 
     float class_multiplier = config.get_class_hp_multiplier(player.get_class());
-
     float race_multiplier = config.get_race_hp_multiplier(player.get_race());
 
-    // Tiene más sentido que el resultado sea un int, se puede cambiar
     return static_cast<int>(player.get_constitution() * class_multiplier * race_multiplier *
                             player.get_level());
 }
@@ -25,15 +25,14 @@ int GameFormulas::calculate_max_mana(const Player& player) {
     const GameConfig& config = GameConfig::get_instance();
 
     float class_multiplier = config.get_class_mana_multiplier(player.get_class());
-
     float race_multiplier = config.get_race_mana_multiplier(player.get_race());
 
-    // Tiene más sentido que el resultado sea un int, se puede cambiar
     return static_cast<int>(player.get_intelligence() * class_multiplier * race_multiplier *
                             player.get_level());
 }
 
 int GameFormulas::calculate_max_gold(const Player& player) {
-    // Tiene más sentido que el resultado sea un int, se puede cambiar
-    return static_cast<int>(100 * std::pow(player.get_level(), 1.1));
+    const GameConfig& config = GameConfig::get_instance();
+    return static_cast<int>(config.get_gold_max_safe_base() *
+                            std::pow(player.get_level(), config.get_gold_max_safe_exp()));
 }
--- server/gameloop_thread.cpp
+++ server/gameloop_thread.cpp
@@ -3,12 +3,10 @@
 #include <chrono>
 #include <iostream>
 
+#include "game/game_config.h"
 #include "game/match.h"
 #include "server.h"
 #include "world/world.h"
-
-#define GAME_LOOP_SLEEP_MS 33  // ~30 loops/seg aprox
-
 
 GameLoopThread::GameLoopThread(Server& server, World& world): world(world), server(server) {}
 
@@ -17,7 +15,8 @@ void GameLoopThread::run() {
         while (should_keep_running()) {
             server.for_each_match([this](Match& match) { match.tick(world); });
 
-            std::this_thread::sleep_for(std::chrono::milliseconds(GAME_LOOP_SLEEP_MS));
+            int sleep_ms = GameConfig::get_instance().get_server_game_loop_sleep_ms();
+            std::this_thread::sleep_for(std::chrono::milliseconds(sleep_ms));
         }
     } catch (const std::exception& e) {
         std::cerr << "[GAMELOOP] Error: " << e.what() << "\n";
