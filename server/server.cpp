45cea28b5586bf1cfea091d96802d7af1af6941b
paula <pconsoli@fi.uba.ar>
2026-06-03 00:13:22 -0300

    fix: solo limpio nick en remove_client si se desconectó sin pasar por disconnect()

--- server/server.cpp
+++ server/server.cpp
@@ -195,9 +195,13 @@ void Server::add_client(PlayerConnection* conn) {
 void Server::remove_client(PlayerConnection* conn) {
     std::lock_guard<std::mutex> lk(clients_mutex);
     clients.remove(conn);
-    const std::string& nick = conn->get_nick();
-    if (!nick.empty()) {
-        nicks_in_use.erase(nick);
+    if (conn->get_state() !=
+        PlayerConnection::State::DISCONNECTING) {  // si el cliente se desconectó inesperadamente
+                                                   // sin pasar por disconnect()
+        const std::string& nick = conn->get_nick();
+        if (!nick.empty()) {
+            nicks_in_use.erase(nick);
+        }
     }
 }
 
