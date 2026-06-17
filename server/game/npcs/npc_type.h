7dd49b4798160743dfe86bd554999707d069be89
rbruno22 <rbruno@libgot.com>
2026-06-17 18:04:18 -0300

    add  npctype

--- server/game/npcs/npc_type.h
+++ server/game/npcs/npc_type.h
@@ -1,23 +1,9 @@
-#ifndef NPC_TYPE_H
-#define NPC_TYPE_H
+#ifndef SERVER_GAME_NPCS_NPC_TYPE_SHIM_H
+#define SERVER_GAME_NPCS_NPC_TYPE_SHIM_H
 
-#include <cstdint>
-
-enum class NPCType : uint32_t {
-    UNKNOWN = 0,
-    PRIEST = 1,
-    MERCHANT = 2,
-    BANKER = 3,
-    GOBLIN = 10,
-    GOBLIN_EXECUTIONER = 11,
-    SPIDER = 12,
-    SPIDER_GIANT = 13,
-    SKELETON = 14,
-    SKELETON_WARRIOR = 15,
-    ZOMBIE = 16,
-    ORC = 17,
-    GOLEM = 18,
-    GOLEM_IRON = 19
-};
+// El enum NPCType vive ahora en common/npc_type.h para que sea compartido
+// con el cliente (deserializa el NPCSnapshot.npc_type). Este header es un
+// shim que se mantiene por compatibilidad con includes viejos.
+#include "common/npc_type.h"
 
 #endif
