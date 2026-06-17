c48991f668a3bfc22716b3815db8b4dfbe8daa4e
rbruno22 <rbruno@libgot.com>
2026-06-17 18:09:23 -0300

    fix

--- server/game/npcs/npc_type.h
+++ server/game/npcs/npc_type.h
@@ -1,9 +1,6 @@
 #ifndef SERVER_GAME_NPCS_NPC_TYPE_SHIM_H
 #define SERVER_GAME_NPCS_NPC_TYPE_SHIM_H
 
-// El enum NPCType vive ahora en common/npc_type.h para que sea compartido
-// con el cliente (deserializa el NPCSnapshot.npc_type). Este header es un
-// shim que se mantiene por compatibilidad con includes viejos.
 #include "common/npc_type.h"
 
 #endif
